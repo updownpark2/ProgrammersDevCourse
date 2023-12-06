969e889e6c91f76e1f826c6e2dfde51c8246c9f3
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2023-12-06 20:16:13 +0900

    라이브러리 설치 학습

--- Practice_Back/Node-Base/figlet-demo.js
+++ Practice_Back/Node-Base/figlet-demo.js
@@ -1,6 +1,8 @@
 var figlet = require("figlet");
+// figlet이라는 모듈을 가지고옵니다.
+// require => 외부모듈을 가져올때 쓰는 함수
 
-figlet("Hello World!!", function (err, data) {
+figlet("SSIBAL", function (err, data) {
   if (err) {
     console.log("Something went wrong...");
     console.dir(err);
